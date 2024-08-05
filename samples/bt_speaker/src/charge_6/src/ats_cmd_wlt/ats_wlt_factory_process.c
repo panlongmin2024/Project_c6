3569132cacf96fe123dd5285c6cfe51e85a54317
panlongmin <lm.pan@worldelite.net.cn>
2024-08-05 17:20:05 +0800

    123

--- samples/bt_speaker/src/charge_6/src/ats_cmd_wlt/ats_wlt_factory_process.c
+++ samples/bt_speaker/src/charge_6/src/ats_cmd_wlt/ats_wlt_factory_process.c
@@ -361,7 +361,7 @@ int ats_wlt_init(void)
 	}
 
 	trace_dma_print_set(false);//disable system printf
-	ret = ats_wlt_uart_init(p_ats_info->ats_uart_dev);
+	//ret = ats_wlt_uart_init(p_ats_info->ats_uart_dev);
 
     p_ats_info->enabled = true;
 
