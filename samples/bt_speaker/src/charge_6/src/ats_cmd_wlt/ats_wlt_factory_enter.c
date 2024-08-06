945ea09bdaa6ac32c6b718cb013e98c2462eae04
panlongmin <lm.pan@worldelite.net.cn>
2024-08-06 20:35:35 +0800

    24

--- samples/bt_speaker/src/charge_6/src/ats_cmd_wlt/ats_wlt_factory_enter.c
+++ samples/bt_speaker/src/charge_6/src/ats_cmd_wlt/ats_wlt_factory_enter.c
@@ -107,9 +107,9 @@ int ats_wlt_enter(void)
 	int ret = -1;
 	SYS_LOG_INF("check wlt ats ! odm=%d\n",ReadODM());
 	
-	if(ReadODM() == 1){
+	if(ReadODM() == 0){
 		k_sleep(20);
-		if(ReadODM() == 1){
+		if(ReadODM() == 0){
 			/* is wlt factory test ! */
 
 			isWltAtsMode_readIO = true;
