86c60f862307a9127e0ed96e9bbadb683d84c371
panlongmin <lm.pan@worldelite.net.cn>
2024-08-17 11:34:06 +0800

    sfd

--- samples/bt_speaker/src/charge_6/src/ats_cmd_wlt/ats_wlt_factory_process.c
+++ samples/bt_speaker/src/charge_6/src/ats_cmd_wlt/ats_wlt_factory_process.c
@@ -126,12 +126,11 @@ static int ats_wlt_uart_deinit(struct device *dev)
 static void ats_wlt_enter_success(struct device *dev, u8_t *buf, int len)
 {
 	ats_wlt_write_data(ATS_SEND_ENTER_WLT_ATS_ACK,sizeof(ATS_SEND_ENTER_WLT_ATS_ACK)-1);
-	
 	isWltAtsMode = true;
-	int mcu_ui_send_led_code(uint8_t type, int code);
+	/*int mcu_ui_send_led_code(uint8_t type, int code);
 	void ls8a10049t_poweron_hold(void);
 	mcu_ui_send_led_code(0x18,1);
-	ls8a10049t_poweron_hold();
+	ls8a10049t_poweron_hold();*/
 }
 bool ats_wlt_get_enter_state(void)
 {
