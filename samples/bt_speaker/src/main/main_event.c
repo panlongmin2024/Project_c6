b166f4ae6e3fbab3e123af2212564619940063f0
panlongmin <lm.pan@worldelite.net.cn>
2024-08-28 17:20:50 +0800

    aaa

--- samples/bt_speaker/src/main/main_event.c
+++ samples/bt_speaker/src/main/main_event.c
@@ -135,8 +135,18 @@ void main_input_event_handle(struct app_msg *msg)
 #ifdef CONFIG_BT_SELF_APP
 		selfapp_config_reset();
 #endif
-		sys_event_notify(SYS_EVENT_POWER_OFF);
-		system_restore_factory_config();
+		char buf[2]={0};
+		int ret = property_get(CFG_USER_ATS_REBOOT_SYSTEM,buf,1);
+		printf("------> ret %d reboot_dat %d\n",ret,buf[0]);
+		if(buf[0] == 6){
+			/* ATS set reboot flag, need reboot */
+			system_restore_factory_config();
+			sys_pm_reboot(0);
+		}
+		else{
+			sys_event_notify(SYS_EVENT_POWER_OFF);
+			system_restore_factory_config();			
+		}
 		break;
 	case MSG_KEY_SWITCH_APP:
 		if (bt_manager_tws_get_dev_role() != BTSRV_TWS_SLAVE) {
